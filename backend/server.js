30939fdc268c11f1a13fa1cdea5edfc106538ea2
Neylan-Body <neylan2011@gmail.com>
2021-05-15 00:36:43 +0000

    update2

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,28 @@ const app = express()
 app.use(cors())
 const port = 3000
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.post('/', async (req, res) => {
-    console.log(req.body)
-    //const {latitude, longitude} = req.body
-    var data = new Date();
-    var hora = data.getHours()+":"+ data.getMinutes()+":"+ data.getSeconds();    
-    // res.json({ latitude, longitude, data, hora });
+    const {latitude, longitude} = req.body
+    var data = new Date()
+    var segundos = data.getSeconds()
+    var minutos = data.getMinutes();
+    var horas = data.getHours()
+    if(segundos<10){
+        segundos = "0"+data.getSeconds()
+    }
+    if(minutos<10){
+        minutos = "0"+data.getMinutes()
+    }
+    if(horas<10){
+        horas = "0"+data.getHours()
+    }
+    var horario = horas+":"+minutos+":"+segundos;
+    console.log(horario);
+    res.json({latitude, longitude, horario});
 })
 
 app.listen(port, () => {
